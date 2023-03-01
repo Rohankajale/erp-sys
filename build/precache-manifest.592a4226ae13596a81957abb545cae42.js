55983eb4eeae87f969a3fa3eeeac77cabed625d9
Rohankajale <rohankajale0@gmail.com>
2023-03-01 18:40:15 +0530

    init

---
+++ build/precache-manifest.592a4226ae13596a81957abb545cae42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cdc906349e493f509f9d676eaa25b35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "feba22615f54158a92df",
+    "url": "/static/css/main.5b1fb4e8.chunk.css"
+  },
+  {
+    "revision": "48270337cffabfe6b176",
+    "url": "/static/js/2.0d198829.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.0d198829.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "feba22615f54158a92df",
+    "url": "/static/js/main.ce40456c.chunk.js"
+  },
+  {
+    "revision": "4f6e34b3be3410815ed1",
+    "url": "/static/js/runtime-main.07b96789.js"
+  },
+  {
+    "revision": "b66532e1f466444fd910f4038c2ccecc",
+    "url": "/static/media/green-chameleon-s9CC2SKySJM-unsplash.b66532e1.jpg"
+  }
+]);
---
+++ office_management/frontend/src/Pages/Feedback/Feedback.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function FeedbackLink() {
+  return (
+    <a href="https://www.example.com/feedback">Give Feedback</a>
+  );
+}
+
+export default FeedbackLink;
