c0f8362e9bf679b77bfeb6f559ef337df57470f7
Rohankajale <rohankajale0@gmail.com>
2023-04-10 09:24:01 +0530

    fix route

--- office_management/frontend/src/Pages/Faculty/FacultyFeedbackLink.jsx
+++ office_management/frontend/src/Pages/Faculty/FacultyFeedbackLink.jsx
@@ -3,7 +3,7 @@ import {  withRouter } from 'react-router-dom'
 import React from 'react';
 
 const FacultyFeedbackLink = () => {
-    return <a href="http://127.0.0.1:5978/login" style={{display: 'block', height: '100vh', width: '100vw', backgroundColor: 'black',borderStyle: 'solid', bottom: '10px',left: '10px', top: '100px',borderWidth: '5px',color: 'teal', fontFamily: 'monospace',fontSize: '70px', textAlign: 'center'}}>Feedback:For assessing students feedback</a>
+    return <a href="http://127.0.0.1:5978/admin?" style={{display: 'block', height: '100vh', width: '100vw', backgroundColor: 'black',borderStyle: 'solid', bottom: '10px',left: '10px', top: '100px',borderWidth: '5px',color: 'teal', fontFamily: 'monospace',fontSize: '70px', textAlign: 'center'}}>Feedback:For assessing students feedback</a>
 }
 
 export default withRouter(FacultyFeedbackLink)
