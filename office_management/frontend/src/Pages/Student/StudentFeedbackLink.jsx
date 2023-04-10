922905b0bbec6c854779df4068202ec687285862
Rohankajale <rohankajale0@gmail.com>
2023-04-10 09:14:01 +0530

    fix css

--- office_management/frontend/src/Pages/Student/StudentFeedbackLink.jsx
+++ office_management/frontend/src/Pages/Student/StudentFeedbackLink.jsx
@@ -3,7 +3,7 @@ import {  withRouter } from 'react-router-dom'
 import React from 'react';
 
 const StudentFeedbackLink = () => {
-    return <a href="http://127.0.0.1:5978" style={{color: 'teal', fontSize: '70px', margin: '15px 10px 1500px 25px'}}>Feedback Form: For Submitting Feedback</a>
+    return <a href="http://127.0.0.1:5978" style={{display: 'block', height: '100vh', width: '100vw', backgroundColor: 'black',borderStyle: 'solid', bottom: '10px',left: '10px', top: '100px',borderWidth: '5px',color: 'teal', fontFamily: 'monospace',fontSize: '70px', textAlign: 'center'}}>Feedback Form: For Submitting Feedback</a>
 }
 
 export default withRouter(StudentFeedbackLink)
