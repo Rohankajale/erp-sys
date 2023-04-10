678ab1200fdd270329c23e5a4a356c5c4d132da0
Rohankajale <rohankajale0@gmail.com>
2023-04-10 09:17:33 +0530

    fix route

--- office_management/frontend/src/Pages/Admin/AdminFeedbackLink.jsx
+++ office_management/frontend/src/Pages/Admin/AdminFeedbackLink.jsx
@@ -3,7 +3,7 @@ import {  withRouter } from 'react-router-dom'
 import React from 'react';
 
 const AdminFeedbackLink = () => {
-    return <a href="http://127.0.0.1:5978" style={{display: 'block', height: '100vh', width: '100vw', backgroundColor: 'black',borderStyle: 'solid', bottom: '10px',left: '10px', top: '100px',borderWidth: '5px',color: 'teal', fontFamily: 'monospace',fontSize: '70px', textAlign: 'center'}}>Feedback: For assessing students feedback</a>
+    return <a href="http://127.0.0.1:5978/login" style={{display: 'block', height: '100vh', width: '100vw', backgroundColor: 'black',borderStyle: 'solid', bottom: '10px',left: '10px', top: '100px',borderWidth: '5px',color: 'teal', fontFamily: 'monospace',fontSize: '70px', textAlign: 'center'}}>Feedback: For assessing students feedback</a>
 }
 //xhr - promises asunc problem
 export default withRouter(AdminFeedbackLink)
